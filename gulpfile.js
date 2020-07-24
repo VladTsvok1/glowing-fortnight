d7c95f9e747705461952bf5811c02fb66e283fd7
Vlad <tsvok.v1@gmail.com>
2020-07-24 16:33:49 +0300

    project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 
 const sass = require('gulp-sass');
 
-const js =require('gulp-js')
 
 function style (){
     return gulp.src('./scss/**/*.scss').pipe(sass()).pipe(gulp.dest('./css'));
