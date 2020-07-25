04be893166a0480a88ca5d1abbac2e0fac516766
Vlad <tsvok.v1@gmail.com>
2020-07-26 01:48:05 +0300

    photo-slider with adaptiwe part of map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
-    $('.section-promo-slider').slick();
+    $('.section-promo-slider').slick({
+        infinite: false,
+        draggable: false,
+        asNavFor: '.section-promo-slider-nav',
+        variableWidth: true,
+    });
+
+    $('.section-promo-slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.section-promo-slider',
+        centerMode: true,
+        focusOnSelect: true,
+        infinite: false,
+        variableWidth: true
+      });
+
+// show slider
+
+
+
+const photoBlock = document.querySelector('.section-promo-media-photos')
+const closeButton = document.querySelector('.slider-close');
+const sliderBlock = document.querySelector('.section-promo-photos-bg')
+photoBlock.addEventListener('click', showSlider)
+closeButton.addEventListener('click', hideSlider)
+
+
+function showSlider(){
+   
+    sliderBlock.classList.add('show-slider');
+}
+function hideSlider(){
+    sliderBlock.classList.remove('show-slider');
+}
+
 })
+
