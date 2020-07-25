a480999748c374f7db420f3a128a2a5620f68ade
Vlad <tsvok.v1@gmail.com>
2020-07-25 18:33:03 +0300

    project structure

--- js/menu-display.js
+++ js/menu-display.js
@@ -1,2 +1,20 @@
+let menuEL = document.querySelector('.section-header-menu_list span')
+menuEL.addEventListener('click', displayMenu);
+document.addEventListener('keydown', hideMenu);
 
-console.log( document.querySelector('.section-header-menu_list span'));
+function displayMenu() {
+
+    let = menuBlock = document.querySelector('.section-menu');
+    menuBlock.classList.add('section-menu-display-js');
+
+}
+
+function hideMenu() {
+    if (document.querySelector('.section-menu').classList.contains('section-menu-display-js')) {
+        if(event.key == 'Escape'){
+            let = menuBlock = document.querySelector('.section-menu');
+            menuBlock.classList.remove('section-menu-display-js');
+        }
+    }
+
+}
