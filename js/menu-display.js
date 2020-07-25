ad9eed160630527480a6a42c87e089db0a4a10e9
Vlad <tsvok.v1@gmail.com>
2020-07-25 18:59:09 +0300

    display menu nad tel

--- js/menu-display.js
+++ js/menu-display.js
@@ -1,10 +1,10 @@
-let menuEL = document.querySelector('.section-header-menu_list span')
+const menuEL = document.querySelector('.section-header-menu_list span')
 menuEL.addEventListener('click', displayMenu);
 document.addEventListener('keydown', hideMenu);
 
 function displayMenu() {
 
-    let = menuBlock = document.querySelector('.section-menu');
+    const  menuBlock = document.querySelector('.section-menu');
     menuBlock.classList.add('section-menu-display-js');
 
 }
@@ -12,7 +12,7 @@ function displayMenu() {
 function hideMenu() {
     if (document.querySelector('.section-menu').classList.contains('section-menu-display-js')) {
         if(event.key == 'Escape'){
-            let = menuBlock = document.querySelector('.section-menu');
+            const  menuBlock = document.querySelector('.section-menu');
             menuBlock.classList.remove('section-menu-display-js');
         }
     }
